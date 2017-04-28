0e7e5bbac3a83cb7dfefb0d49bd77edeab2abeaf
nickyzhi <zhiqiyubupt@gmail.com>
2017-04-28 01:42:38 -0400

    click rect

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,7 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
+
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
