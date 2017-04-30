5ff735a2cba68438d4f026d3f84bc566df2c7758
Shuai He <hs.heshuai@gmail.com>
2017-04-30 14:52:29 -0400

    Add draggable profile image feature

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,28 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 
+// drag profile circle
+function rotateAnnotationCropper(offsetSelector, xCoordinate, yCoordinate, cropper){
+        //alert(offsetSelector.left);
+    
+        var x = xCoordinate - offsetSelector.offset().left - offsetSelector.width()/2;
+        var y = -1*(yCoordinate - offsetSelector.offset().top - offsetSelector.height()/2);
+        var theta = Math.atan2(y,x)*(180/Math.PI);        
+
+
+        var cssDegs = convertThetaToCssDegs(theta);
+        var rotate = 'rotate(' +cssDegs + 'deg)';
+        cropper.css({'-moz-transform': rotate, 'transform' : rotate, '-webkit-transform': rotate, '-ms-transform': rotate});
+        
+        $('body').on('mouseup', function(event){ $('body').unbind('mousemove')}); 
+        $('body').on('touchend', function(event){ $('body').unbind('touchmove')}); 
+
+}
+
+function convertThetaToCssDegs(theta){
+    var cssDegs = 90 - theta;
+    return cssDegs;
+}
 
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
